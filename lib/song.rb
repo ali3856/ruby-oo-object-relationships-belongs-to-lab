394440bd47749e25ee315dd70314284ecdb5c5fc
Asim Ali <asimsali95@gmail.com>
2021-08-01 22:12:48 -0500

    commit tree 964f63e6fc5ae522c5c57cec0cbe4a4eb27177e0

---
+++ lib/song.rb
@@ -0,0 +1,7 @@
+class Song
+    attr_accessor :title, :artist #by adding :aritst this class belongs to artist - dont know what this means yet but means that these two classes are connected, maybe dependent of one another
+    
+    def initialize
+        @title = title
+    end
+end
